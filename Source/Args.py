63b7d017a24f7e3019874024bfd1409f0b3ab3d1
Hai Do <dodinhhai29904@gmail.com>
2024-04-05 22:29:58 +0700

    change rule for Args

--- Source/Args.py
+++ Source/Args.py
@@ -4,6 +4,7 @@ import Graphic
 import Game
 import random
 import importlib
+import os
 
 def get_args():
     args = sys.argv
@@ -16,28 +17,36 @@ def get_args():
         args_dict[key] = value
     return args_dict
 
+def get_file_names():
+    Tests_path = os.path.join(os.getcwd(), "Tests")
+    filename_pool = os.listdir(Tests_path)
+    return filename_pool
+
 def change_global_vars(args):
     global MODE, WIDTH, HEIGHT, LEVEL, FILENAME, FPS, ITERATION
-    FILENAME_POOL = ["map1_1.txt", "map1.txt", "map2.txt", "map3.txt", "map4.txt","map5.txt", "maze.txt",
-                    "maze1.txt", "maze2.txt", "maze3.txt", "maze4.txt", "maze5.txt", "maze6.txt", "maze7.txt",
-                    "maze8.txt", "maze9.txt", "maze10.txt", "maze11.txt", "maze12.txt"]
     for key, value in args.items():
         if key == "MODE":
             MODE = value
+            if MODE == "RANDOM":
+                MODE = random.choice(["DARK", "LIGHT", "DUNE"])
         if key == "WIDTH":
             WIDTH = int(value)
         if key == "HEIGHT":
             HEIGHT = int(value)
         if key == "LEVEL":
             LEVEL = value
+            if LEVEL == "RANDOM":
+                LEVEL = random.choice(["1", "2", "3"])
         if key == "FILENAME":
             FILENAME = value
             if FILENAME == "RANDOM":
-                FILENAME = "Tests/" + random.choice(FILENAME_POOL)
+                FILENAME = "Tests/" + random.choice(get_file_names())
         if key == "FPS":
             FPS = int(value)
         if key == "ITERATION":
             ITERATION = int(value)
+            if ITERATION <= 0:
+                ITERATION = random.randint(1, 1000)
     importlib.reload(Global)
     importlib.reload(Graphic)
     importlib.reload(Game)
